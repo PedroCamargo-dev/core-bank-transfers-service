c2a82c04da37fb28804677336e2ea1b8b18dfd82
Pedro Camargo <pedrocamargo3008@gmail.com>
2026-01-10 22:20:16 -0300

    refactor: cleanup error handling and update mocks generation

--- internal/ports/gateway/mocks/doc.go
+++ internal/ports/gateway/mocks/doc.go
@@ -1,5 +1,4 @@
-//go:build ignore
-
+// Package mocks provides mock implementations for testing purposes.
 package mocks
 
 //go:generate mockgen -destination=mock_persistence.go -package=mocks github.com/PedroCamargo-dev/core-bank-transfers-service/internal/ports/gateway/persistence TransferRepository,OutboxRepository,InboxRepository,UnitOfWork
